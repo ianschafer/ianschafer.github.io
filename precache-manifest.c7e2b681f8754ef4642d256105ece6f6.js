3e6b02733c8f2e009e3f5ce3bf1df8efcb4516af
Spencer Schafer <spencerschafer@gmail.com>
2020-08-16 21:56:27 +0200

    Add files via upload

---
+++ precache-manifest.c7e2b681f8754ef4642d256105ece6f6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "71b7dfeaedbb41f336450328a4bf9aa8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "543965f76064e89569bf",
+    "url": "/static/css/main.9a9c522d.chunk.css"
+  },
+  {
+    "revision": "d9458a6e15d34417669a",
+    "url": "/static/js/2.9f7fe0d4.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.9f7fe0d4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "543965f76064e89569bf",
+    "url": "/static/js/main.819cb258.chunk.js"
+  },
+  {
+    "revision": "ca6e683cf2978694ca77",
+    "url": "/static/js/runtime-main.22ef8554.js"
+  }
+]);
